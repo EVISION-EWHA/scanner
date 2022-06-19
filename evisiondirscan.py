b36f19deb350832be7ced4074ddb19f5b439e5ba
songah119 <skutcm119@gmail.com>
2022-06-20 05:56:00 +0900

    [init] 라이브러리 import

---
+++ evisiondirscan.py
@@ -0,0 +1,9 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QHBoxLayout, QVBoxLayout, QMessageBox, QLabel, QLineEdit, QTextBrowser, QGridLayout, QTextEdit, QDesktopWidget
+
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import QCoreApplication, Qt
+
+import os
+import re
+from pprint import pprint
